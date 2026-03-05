88f47219dc3cd50f8fb207ff52d22d473a4b0262
MAZIN AHMED <mazinahmedbafatia.coderkube@gmail.com>
2026-03-05 11:12:47 +0530

    Update project files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+ <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
